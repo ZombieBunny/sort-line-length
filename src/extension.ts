b20eeddcdd3578cf2fa8279ffd515c2c64dd6670
Lee <leevashan.reddi@aax.co.za>
2023-07-31 17:08:52 +0200

    wip

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,15 @@
 import * as vscode from 'vscode'
+
 export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('extension.sortLineLength', () => {
 		const editor = vscode.window.activeTextEditor;
+
+		// identify language
+		// define sorting strategy for multiline support
+		// apply sorting logic based on language
+		const language = editor?.document.languageId;
+
+
 		const selection = editor!.selection;
 
 		if (selection && selection.isEmpty) {
@@ -18,11 +26,20 @@ export function activate(context: vscode.ExtensionContext) {
 				lines.push(text);
 			}
 
+
+			const huh = groupLines(lines);
+			// flattern lines into single line string
 			const beforeLines = JSON.parse(JSON.stringify(lines.join(",").toString()));
+
+			// then sort lines 
 			lines.sort(function (a, b) { return b.length - a.length });
+
+			// then flatten sorted lines into single line
 			const afterLines = JSON.parse(JSON.stringify(lines.join(",").toString()));
 
-			// after sorting, if lines match, then change direction of sort
+			// after sorting, if lines match, then change direction of sort.
+			// this means that the user has triggered the event while the same text is highlighted.
+			// we just want to invrert sorting in thsi scenario.
 			if (beforeLines === afterLines) {
 				lines.sort(function (a, b) { return a.length - b.length });
 			}
@@ -41,6 +58,29 @@ export function activate(context: vscode.ExtensionContext) {
 
 	context.subscriptions.push(disposable);
 }
+
+// Define multi line support:
+// JS/TS
+// identify line start and line end.
+// - use regex ?? : ^import\s+{[\s\S]*?}\s+from\s+'[\s\S]*?';
+// string.includes = ['import','const'];
+// string.includes = ['from','requires',';'];
+const groupLines = (lines: string[]): string => {
+	// create multi
+	const line = JSON.parse(JSON.stringify(lines.join("|").toString()));
+	const regex = /import\s+{[\s\S]?(\|[\s\S]+?)}\s+from\s+'[\w\s\S]*?';/g;
+
+	const matches = line.match(regex);
+	if (matches) {
+		console.log("Found multiline text:");
+		console.log(matches);
+	} else {
+		console.log("No multiline text found.");
+	}
+
+	return "";
+}
+
 
 // this method is called when your extension is deactivated
 export function deactivate() { }
